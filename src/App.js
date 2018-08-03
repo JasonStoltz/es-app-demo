1a5aaaf2e08315064b5aab89b36420abd2745750
Jason Stoltzfus <jastoltz24@gmail.com>
2018-08-03 14:20:20 -0400

    Added an initial search box

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  SearchkitManager,
+  SearchkitProvider,
+  SearchBox,
+  MultiMatchQuery,
+  Hits
+} from "searchkit";
 
+import "./App.css";
+
+const searchFields = [
+  "license",
+  "name",
+  "repository",
+  "dependencies",
+  "keywords",
+  "description",
+  "id",
+  "version",
+  "owners"
+];
+
+const searchkit = new SearchkitManager("http://localhost:9200/");
+
+searchkit.addDefaultQuery(query => {
+  return query.addQuery(
+    MultiMatchQuery("", {
+      fields: searchFields,
+      operator: "OR"
+    })
+  );
+});
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SearchkitProvider searchkit={searchkit}>
+        <div className="App">
+          <SearchBox
+            queryBuilder={term => {
+              return MultiMatchQuery(term, {
+                fields: searchFields,
+                operator: "OR"
+              });
+            }}
+          />
+          <Hits />
+        </div>
+      </SearchkitProvider>
     );
   }
 }
